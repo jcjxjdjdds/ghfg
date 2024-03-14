33833049a327c3e8f9fe725484123a386fbc2994
jcjxjdjdds <104429826+jcjxjdjdds@users.noreply.github.com>
2024-03-14 22:45:05 +0200

    Update OWNER.py

--- OWNER.py
+++ OWNER.py
@@ -5,6 +5,6 @@ BOT_TOKEN = "6875562036:AAHA02aA0LszliIr7RbkeLXGJmOeAS9klo4"
 DATABASE = "mongodb+srv://Elkber:Elkber@cluster0.feuljpn.mongodb.net/?retryWrites=true&w=majority"
 CHANNEL = "https://t.me/HLV_M"
 GROUP = "https://t.me/ALTUNZAT"
-VIDEO = "https://t.me/HLV_M/720"
+VIDEO = "https://t.me/HLV_M/999"
 LOGS = "SAGAL_botk"
 
